ed0a101e511f31031b65a1d4270569935cab5584
hhj1897 <js1907@imperial.ac.uk>
2021-10-23 18:14:39 +0100

    Update the test script.

--- face_alignment_test.py
+++ face_alignment_test.py
@@ -2,10 +2,15 @@ import os
 import cv2
 import time
 import torch
+import numpy as np
 from argparse import ArgumentParser
 from ibug.face_alignment import FANPredictor
 from ibug.face_alignment.utils import plot_landmarks
 from ibug.face_detection import RetinaFacePredictor, S3FDPredictor
+try:
+    from scipy.interpolate import CubicSpline
+except:
+    CubicSpline = None
 
 
 def main() -> None:
@@ -19,6 +24,7 @@ def main() -> None:
                         action='store_true', default=False)
     parser.add_argument('--no-display', '-n', help='No display if processing a video file',
                         action='store_true', default=False)
+    parser.add_argument('--eyes-of-ibad', '-eoi', help='A tribute to Dune', type=float, default=0.0)
 
     parser.add_argument('--detection-threshold', '-dt', type=float, default=0.8,
                         help='Confidence threshold for face detection (default=0.8)')
@@ -29,15 +35,19 @@ def main() -> None:
                              'can be either resnet50 or mobilenet0.25 when using RetinaFace')
     parser.add_argument('--detection-device', '-dd', default='cuda:0',
                         help='Device to be used for face detection (default=cuda:0)')
+    parser.add_argument('--hide-detection-results', '-hd', help='Do not visualise face detection results',
+                        action='store_true', default=False)
 
     parser.add_argument('--alignment-threshold', '-at', type=float, default=0.2,
-                        help='Score threshold used when visualising detected landmarks (default=0.2)'),
+                        help='Score threshold used when visualising detected landmarks (default=0.2)')
     parser.add_argument('--alignment-method', '-am', default='fan',
                         help='Face alignment method, must be set to FAN')
     parser.add_argument('--alignment-weights', '-aw', default=None,
                         help='Weights to be loaded for face alignment, can be either 2DFAN2 or 2DFAN4')
     parser.add_argument('--alignment-device', '-ad', default='cuda:0',
                         help='Device to be used for face alignment (default=cuda:0)')
+    parser.add_argument('--hide-alignment-results', '-ha', help='Do not visualise face alignment results',
+                        action='store_true', default=False)
     args = parser.parse_args()
 
     # Set benchmark mode flag for CUDNN
@@ -116,11 +126,43 @@ def main() -> None:
                       f'{elapsed_time2 * 1000.0:.04f} ms: {len(faces)} faces analysed.')
 
                 # Rendering
+                if args.eyes_of_ibad > 0:
+                    contours = []
+                    for lm, sc in zip(landmarks, scores):
+                        if CubicSpline is None:
+                            if sc[36:42].min() >= args.alignment_threshold:
+                                contours.append(lm[36:42].astype(int))
+                            if sc[42:48].min() >= args.alignment_threshold:
+                                contours.append(lm[42:48].astype(int))
+                        else:
+                            if sc[36:42].min() >= args.alignment_threshold:
+                                upper_lid = CubicSpline(range(4), lm[36:40])
+                                lower_lid = CubicSpline(range(4), np.vstack((lm[39:42], lm[36])))
+                                upper_lid_pts = upper_lid(np.arange(0, 3.0, 0.1))
+                                lower_lid_pts = lower_lid(np.arange(0, 3.0, 0.1))
+                                contours.append(np.vstack((upper_lid_pts, lower_lid_pts)).astype(int))
+                            if sc[42:48].min() >= args.alignment_threshold:
+                                upper_lid = CubicSpline(range(4), lm[42:46])
+                                lower_lid = CubicSpline(range(4), np.vstack((lm[45:48], lm[42])))
+                                upper_lid_pts = upper_lid(np.arange(0, 3.0, 0.1))
+                                lower_lid_pts = lower_lid(np.arange(0, 3.0, 0.1))
+                                contours.append(np.vstack((upper_lid_pts, lower_lid_pts)).astype(int))
+                    if len(contours) > 0:
+                        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV_FULL)
+                        temp_hsv_frame = hsv_frame.copy()
+                        alpha = np.clip(args.eyes_of_ibad, 0, 1)
+                        cv2.fillPoly(temp_hsv_frame, contours, (170, 255, 0), lineType=cv2.LINE_AA)
+                        hsv_frame[..., :2] = temp_hsv_frame[..., :2]
+                        temp_frame = cv2.cvtColor(hsv_frame, cv2.COLOR_HSV2BGR_FULL)
+                        frame = (frame.astype(float) * (1 - alpha) +
+                                 temp_frame.astype(float) * alpha).astype(np.uint8)
                 for face, lm, sc in zip(faces, landmarks, scores):
-                    bbox = face[:4].astype(int)
-                    cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), color=(0, 0, 255), thickness=2)
-                    plot_landmarks(frame, lm, sc, threshold=args.alignment_threshold)
-                    if len(face) > 5:
+                    if not args.hide_detection_results:
+                        bbox = face[:4].astype(int)
+                        cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), color=(0, 0, 255), thickness=2)
+                    if not args.hide_alignment_results:
+                        plot_landmarks(frame, lm, sc, threshold=args.alignment_threshold)
+                    if not args.hide_detection_results and len(face) > 5:
                         plot_landmarks(frame, face[5:].reshape((-1, 2)), pts_radius=3)
 
                 # Write the frame to output video (if recording)
